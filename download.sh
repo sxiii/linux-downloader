e59ebf14df450275f19d1963940cac80c80eb12f
Dennis Ivanov <securityxiii@gmail.com>
2020-08-19 13:35:22 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
-echo "/-------------------------------------------------------------------------------------------------------------------------\ "
-echo "| Script downloads recent (latest release) linux ISOs and spins a VM for a test. This is kinda distrohopper dream machine.| "
-echo "| It consist of the file with distro download functions (distrofunctions.sh) as well as this script.                      | "
-echo "| Theoretically, the script should always download recent linux ISOs without any updates. But, if the developer(s)        | "
-echo "| change the download URL or something else, it might be required to do manual changes - probably in distrofunctions.sh.  | "
-echo "| Requirements: linux, bash, curl, wget, awk, grep, xargs, for guix you'll also need 'xz' as it's compressed /------------/ "
-echo "| Written by SecurityXIII / August 2020 / Kopimi un-license  /-----------------------------------------------/"
+echo "/---------------------------------------------------------------------------------------------------------------------------\ "
+echo "| Script downloads recent (latest release) linux ISOs and spins a VM for a test. This is kinda distrohopper dream machine.  | "
+echo "| It consist of the file with distro download functions (distrofunctions.sh) as well as this script.                        | "
+echo "| Theoretically, the script should always download recent linux ISOs without any updates. But, if the developer(s)          | "
+echo "| change the download URL or something else, it might be required to do manual changes - probably in distrofunctions.sh.    | "
+echo "| Requirements: linux, bash, curl, wget, awk, grep, xargs, paste, column; for guix you'll also need 'xz' as it's compressed | "
+echo "| Written by SecurityXIII / August 2020 / Kopimi un-license  /--------------------------------------------------------------/ "
 echo "\------------------------------------------------------------/"
 echo "+ How to use?"
 echo "If you manually pick distros (opt. one or two) you will be prompted about launching a VM for test spin for each distro."
 echo "Multiple values are also supported. Please choose:"
 echo "* one distribution (e.g. type 0 for archlinux)*"
-echo "* several distros - space separated (e.g. for getting both Arch and Debian, type '0 4')*"
-echo "* 'all' option, the script will ONLY download ALL of the ISOs (warning: this can take a lot of space!)"
+echo "* several distros - space separated (e.g. for getting both Arch and Debian, type '0 4' (without quotes))*"
+echo "* 'all' option, the script will ONLY download ALL of the ISOs (warning: this can take a lot of space (80+GB) !)"
 
 # NB: I wanted to add ElementaryOS but the developers made it way too hard to implement auto-downloading.
 # If you can find constant mirror or place for actual release of ElementaryOS, please do a pull-request or just leave a comment.
 
 # "WIP". Todo:	1. Multiple architecture support;
 #		2. Multiple download mirror support;
-
-# Add other: void, 4mlinux, kaos, clearlinux, dragora, reactos, haiku, freebsd
-# Add sourcebased: pclinuxos, easyos,
-# Test: parabola
+#              3. Show categories for user and allow to download all distros from specific category.	
 
 # Load the functions from distrofunctions.sh:
 . distrofunctions.sh
@@ -31,11 +28,13 @@ echo "* 'all' option, the script will ONLY download ALL of the ISOs (warning: th
 arch=(archlinux manjaro arcolinux archbang parabola)
 deb=(debian ubuntu linuxmint altlinux zorinos solus popos deepin mxlinux knoppix kali puppy pureos)
 rpm=(fedora centos opensuse rosa mandriva mageia clearos)
-other=(alpine tinycore porteus slitaz)
+other=(alpine tinycore porteus slitaz pclinuxos void fourmlinux kaos clearlinux dragora)
 sourcebased=(gentoo sabayon calculate nixos guix)
+containers=(rancheros k3os flatcar silverblue) # What Else?
+notlinux=(freebsd openindiana minix haiku menuetos kolibrios reactos)
 
 # All distributions
-distro_all=("${arch[@]}" "${deb[@]}" "${rpm[@]}" "${other[@]}" "${sourcebased[@]}")
+distro_all=("${arch[@]}" "${deb[@]}" "${rpm[@]}" "${other[@]}" "${sourcebased[@]}" "${containers[@]}" "${notlinux[@]}")
 
 # Legend ## Distroname ## Arch  ## Type     ## Download URL 
 archlinux=("ArchLinux" "amd64" "rolling" "archurl")
@@ -70,6 +69,12 @@ alpine=("Alpine" "amd64" "extended" "alpineurl")
 tinycore=("TinyCore" "amd64" "current" "tinycoreurl")
 porteus=("Porteus" "amd64" "kiosk" "porteusurl")
 slitaz=("SliTaz" "amd64" "rolling" "slitazurl")
+pclinuxos=("PCLinuxOS" "amd64" "livecd" "pclinuxosurl")
+void=("Void" "amd64" "live" "voidurl")
+fourmlinux=("4mlinux" "amd64" "release" "fourmurl")
+kaos=("kaos" "amd64" "release" "kaosurl")
+clearlinux=("ClearLinux" "amd64" "release" "clearurl")
+dragora=("Dragora" "amd64" "release" "dragoraurl")
 
 gentoo=("Gentoo" "amd64" "admincd" "gentoourl")
 sabayon=("Sabayon" "amd64" "daily" "sabayonurl")
@@ -77,8 +82,22 @@ calculate=("Calculate" "amd64" "release" "calcurl")
 nixos=("NixOS" "amd64" "unstable" "nixurl")
 guix=("Guix" "amd64" "release" "guixurl")
 
-for ((i=0; i<${#distro_all[@]}; i++)); do colout+="$i = ${distro_all[$i]} \n"; done
-printf "$colout" | paste - - - - - -
+rancheros=("RancherOS" "amd64" "release" "rancherurl")
+k3os=("K3OS" "amd64" "release" "k3osurl")
+flatcar=("Flatcar" "amd64" "release" "flatcarurl")
+silverblue=("Silverblue" "amd64" "release" "silverblueurl")
+
+freebsd=("FreeBSD" "amd64" "release" "freebsdurl")
+openindiana=("OpenIndiana" "amd64" "release" "indianaurl")
+minix=("MINIX" "amd64" "release" "minixurl")
+haiku=("Haiku" "amd64" "release" "haikuurl")
+menuetos=("MenuetOS" "amd64" "release" "menueturl")
+kolibrios=("KolibriOS" "amd64" "release" "kolibrios")
+reactos=("ReactOS" "amd64" "release" "reactosurl")
+
+for ((i=0; i<${#distro_all[@]}; i++)); do dist=${distro_all[$i]}; typeset -n arr=$dist; colout+="$i = ${arr[0]} \n"; done
+
+printf "$colout" | paste - - - - - - | column -t
 
 echo "Please choose distro to download (type-in number or space-separated multiple numbers):"
 read x
@@ -92,9 +111,8 @@ if [ "$x" != "all" ]; then
 
 	dist=${distro_all[$distr]}
 	typeset -n arr=$dist
-	echo "You choose ${arr[0]}, ${arr[2]}-type distro built for ${arr[1]} arch. Do you want to download ${arr[0]} ISO? (y / n)"
+	echo "You choose ${arr[0]} distro ${arr[2]}, built for ${arr[1]} arch. Do you want to download ${arr[0]} ISO? (y / n)"
 	read z
-	#if [ $z = "y" ]; then $"${arr[3]}"; wget -c $new ; fi
 	if [ $z = "y" ]; then $"${arr[3]}"; fi
 	echo "${arr[0]} downloaded, do you want to spin up the QEMU? (y / n)"
 	read z
@@ -103,9 +121,9 @@ if [ "$x" != "all" ]; then
 	        # ADD QUEMU AVAILABILITY CHECK
 		isoname="$(echo ${arr[0]} | awk '{print tolower($0)}').iso"
 		# Uncomment the following two rows (a1 & a2) and comment out third (b1) if you want to make QEMU HDD
-		#qemu-img create ./${arr[0]}.img 4G                                                # a1. Creating HDD (if you want changes to be preserved)
-		#qemu-system-x86_64 -hda ./${arr[0]}.img -boot d -cdrom ./${arr[0]}*.iso -m 1024   # a2. Booting from CDROM with HDD support (changes will be preserved)
-		qemu-system-x86_64 -boot d -cdrom ./$isoname -m 1024                               # b1. This is liveCD boot without HDD (all changes will be lost)
+		# qemu-img create ./${arr[0]}.img 4G                                                # a1. Creating HDD (if you want changes to be preserved)
+		# qemu-system-x86_64 -hda ./${arr[0]}.img -boot d -cdrom ./${arr[0]}*.iso -m 1024   # a2. Booting from CDROM with HDD support (changes will be preserved)
+		qemu-system-x86_64 -boot d -cdrom ./$isoname -m 1024                                # b1. This is liveCD boot without HDD (all changes will be lost)
 	fi
 
 	done
@@ -113,5 +131,5 @@ else
 
 # Only download will happen if user picked "all" option
 	if [ "$x" = "all" ]; then i=0; for ((i=0; i<${#distro_all[@]}; i++)); do xx+="$i "; done; x=$xx; fi # "All" handling
-	$"${arr[3]}"; #wget -c $new
+	$"${arr[3]}";
 fi
